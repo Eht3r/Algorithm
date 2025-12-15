14740cfa82c503ad81612b5f6ba225f6959f7423
박민규 <ww9986@hs.ac.kr>
2025-12-15 15:02:27 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2668 ms, Memory: 341580 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -4,7 +4,6 @@ import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws Exception {
