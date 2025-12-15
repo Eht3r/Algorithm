f7d54a99a7cdedf5628a32dc3ea3baef3f093102
박민규 <ww9986@hs.ac.kr>
2025-12-15 14:57:55 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 100 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+
+        int day = (V - B - 1) / (A - B) + 1;
+
+        bw.write(String.valueOf(day));
+        bw.flush();
+        bw.close();
+    }
+}
