2761e34913fce8ce2f7d338b6ad103e336aaef4c
박민규 <ww9986@hs.ac.kr>
2025-12-22 16:08:57 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 136 ms, Memory: 24712 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= N; i++) {
+            queue.add(i);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
+
+        while (!queue.isEmpty()) {
+            for (int i = 0; i < K - 1; i++) {
+                queue.add(queue.poll());
+            }
+
+            sb.append(queue.poll());
+
+            if (!queue.isEmpty()) {
+                sb.append(", ");
+            }
+        }
+
+        sb.append(">");
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
