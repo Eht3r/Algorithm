9cd3f7754ef1e913a97c8a3bdc208801c06a28ac
박민규 <ww9986@hs.ac.kr>
2025-12-22 15:37:36 +0900

    [Silver IV] Title: 괄호, Time: 112 ms, Memory: 14312 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < N; i++) {
+            String str = br.readLine();
+            Stack<Character> stack = new Stack<>();
+            boolean isValid = true;
+            
+            for (char c : str.toCharArray()) {
+                if (c == '(') {
+                    stack.push(c);
+                } else {
+                    if (stack.empty()) {
+                        isValid = false;
+                    } else {
+                        stack.pop();
+                    }
+                }
+            }
+
+            if (isValid && stack.empty()) {
+                bw.write("YES\n");
+            } else {
+                bw.write("NO\n");
+            }
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
