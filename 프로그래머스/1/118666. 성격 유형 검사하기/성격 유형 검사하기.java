7f4136b46be7eca41cd53e243736b63077a370a4
박민규 <ww9986@hs.ac.kr>
2026-05-19 14:03:28 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.58 ms, Memory: 59.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class Solution {
+    private static Map <Character, Integer> map = new HashMap<>();
+    
+    public String solution(String[] survey, int[] choices) {
+        StringBuilder answer = new StringBuilder();
+        
+        for (int i = 0; i < survey.length; i++) {
+            if (choices[i] < 4) {
+                char personality = survey[i].charAt(0);
+                int score = 4 - choices[i];
+                map.put(personality, map.getOrDefault(personality, 0) + score);
+            } else if (choices[i] > 4) {
+                char personality = survey[i].charAt(1);
+                int score = choices[i] - 4;
+                map.put(personality, map.getOrDefault(personality, 0) + score);
+            } else {
+                map.put(survey[i].charAt(0), map.getOrDefault(survey[i].charAt(0), 0));
+                map.put(survey[i].charAt(1), map.getOrDefault(survey[i].charAt(1), 0));
+            }
+        }
+        
+        answer.append(compare('R', 'T'))
+            .append(compare('C', 'F'))
+            .append(compare('J', 'M'))
+            .append(compare('A', 'N'));
+        
+        return answer.toString();
+    }
+    
+    private Character compare(Character str1, Character str2) {
+        int score1 = map.getOrDefault(str1, 0);
+        int score2 = map.getOrDefault(str2, 0);
+        
+        if (score1 > score2) {
+            return str1;
+        } else if (score1 < score2) {
+            return str2;
+        } else {
+            return str1.compareTo(str2) <= 0 ? str1 : str2;
+        }
+    }
+}
