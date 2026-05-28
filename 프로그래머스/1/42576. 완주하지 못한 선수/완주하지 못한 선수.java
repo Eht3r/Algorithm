3468a19d2994b9e4e57a377c27d789e81edd89b1
박민규 <ww9986@hs.ac.kr>
2026-05-28 10:39:09 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 39.87 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,24 +2,22 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
-        
-        Map<String, Integer> map = new HashMap<>();
-        for (String par : participant) {
-            map.put(par, map.getOrDefault(par, 0) + 1);
+        Map<String, Integer> participantMap = new HashMap<>(); 
+
+        for (String name : participant) {
+            participantMap.put(name, participantMap.getOrDefault(name, 0) + 1);
         }
         
-        for (String comp: completion) {
-            map.put(comp, map.getOrDefault(comp, 1) - 1);
+        for (String name : completion) {
+            participantMap.put(name, participantMap.get(name) - 1);
         }
         
-        for (String key : map.keySet()) {
-            if (1 == map.get(key)) {
-                answer = key;
+        for (String name : participantMap.keySet()) {
+            if (participantMap.get(name) > 0) {
+                return name;
             }
         }
         
-        
-        return answer;
+        return "";
     }
 }
